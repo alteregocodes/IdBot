fda320ff209668f931f5def2cffd4e9c3d1ab1e5
/AlteregoCodes <alteregocode@kutmut.my.id>
2024-06-20 06:46:11 +0700

    Update config.py

--- config.py
+++ config.py
@@ -8,4 +8,3 @@ OWNER_IDS = [7494727691]  # Hanya satu owner ID
 
 START_MSG = "Halo! Saya adalah bot yang dapat mengambil ID channel/grup dari pesan yang diteruskan."
 UPDATE_LOG_FILE = "update_log.txt"  # Nama file log pembaruan sementara
-HELP_MSG = "Gunakan tombol di bawah untuk melihat perintah yang tersedia."
