4fde6de8fef80ad7c927caed06913053cf574369
/AlteregoCodes <alteregocode@kutmut.my.id>
2024-06-19 22:11:17 +0700

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 
 API_ID = 23795091
 API_HASH = "64a8dadb2a928e4a97e037e834d1f8db"
-BOT_TOKEN = "6196900445:AAHiKSFMwOAG7p4axl5saFWVluTxQHmFLPw"
+BOT_TOKEN = "7283082118:AAHnz0Nh3-mcI1oZQ0gso0eCr6dhxKv1d4o"
 OWNER_ID = 6109394253
 
 START_MSG = "Halo! Saya adalah bot yang dapat mengambil ID channel/grup dari pesan yang diteruskan."
