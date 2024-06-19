b28ab2930cd49aeb5b5f97a7f16a55460cd87629
/AlteregoCodes <alteregocode@kutmut.my.id>
2024-06-20 06:02:31 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -2,14 +2,14 @@
 
 from pyrogram import Client
 from config import API_ID, API_HASH, BOT_TOKEN
-from module import start, update, carbon, getid, help
+from module import start, update, carbon_module, getid, help
 
 app = Client("channel_id_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
 # Inisialisasi modul
 start.init(app)
 update.init(app)
-carbon.init(app)
+carbon_module.init(app)
 getid.init(app)
 help.init(app)
 
