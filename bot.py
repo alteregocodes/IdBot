211163277c696ad14c167fe29d56ecdc0cc592fb
/AlteregoCodes <alteregocode@kutmut.my.id>
2024-06-19 21:09:41 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,44 +1,25 @@
 # bot.py
 
-import logging
-from aiogram import Bot, Dispatcher, types
-from aiogram.types import ParseMode
-from aiogram.utils import executor
-from config import Config
+from pyrogram import Client, filters
+from pyrogram.types import Message
+from config import API_ID, API_HASH, BOT_TOKEN, OWNER_ID, START_MSG
 
-# Konfigurasi logging
-logging.basicConfig(level=logging.INFO)
+app = Client("channel_id_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
-# Inisialisasi bot dan dispatcher
-bot = Bot(token=Config.BOT_TOKEN)
-dp = Dispatcher(bot)
+# Handler untuk memulai bot
+@app.on_message(filters.command("start") & filters.private)
+async def start(client, message: Message):
+    await message.reply_text(START_MSG)
 
-async def is_subscribed(user_id):
-    if Config.FORCE_SUBS_CHANNEL_ID:
-        chat_member = await bot.get_chat_member(Config.FORCE_SUBS_CHANNEL_ID, user_id)
-        return chat_member.status in ['member', 'administrator', 'creator']
-    return True
-
-# Handler untuk perintah /start
-@dp.message_handler(commands=['start'])
-async def send_welcome(message: types.Message):
-    if await is_subscribed(message.from_user.id):
-        await message.reply(Config.START_MESSAGE)
-    else:
-        await message.reply(Config.FORCE_SUBS_MESSAGE.format(channel_link=Config.FORCE_SUBS_CHANNEL_ID))
-
-# Handler untuk pesan yang diteruskan
-@dp.message_handler(content_types=types.ContentType.ANY)
-async def forward_handler(message: types.Message):
-    if not await is_subscribed(message.from_user.id):
-        await message.reply(Config.FORCE_SUBS_MESSAGE.format(channel_link=Config.FORCE_SUBS_CHANNEL_ID))
-        return
-
+# Handler untuk pesan diteruskan
+@app.on_message(filters.forwarded)
+async def get_forwarded_info(client, message: Message):
     if message.forward_from_chat:
-        forward_chat_id = message.forward_from_chat.id
-        await message.reply(f"ID channel/grup yang diteruskan: {forward_chat_id}")
+        chat_id = message.forward_from_chat.id
+        chat_id_formatted = f"-100{abs(chat_id)}" if chat_id < 0 else str(chat_id)
+        await message.reply_text(f"ID Channel/Grup: {chat_id_formatted}")
     else:
-        await message.reply("Pesan ini tidak diteruskan dari channel/grup.")
+        await message.reply_text("Pesan ini tidak berasal dari channel atau grup.")
 
-if __name__ == '__main__':
-    executor.start_polling(dp, skip_updates=True)
+if __name__ == "__main__":
+    app.run()
