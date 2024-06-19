a7068097ea069977958690dabcaf75df540c6de3
/AlteregoCodes <alteregocode@kutmut.my.id>
2024-06-19 21:09:05 +0700

    Update config.py

--- config.py
+++ config.py
@@ -1,20 +1,9 @@
 # config.py
 
-class Config:
-    # Data bot
-    BOT_TOKEN = '7429042824:AAG_4iwiO15vYqOCxr9Cn9ygnAG-1vNSzys'
-    
-    # Data owner
-    OWNER_ID = 6109394253  # Ganti dengan ID Telegram Anda
-    
-    # Pesan start
-    START_MESSAGE = "Halo! Saya adalah bot untuk mendapatkan ID channel/grup dari pesan yang diteruskan.\n\n" \
-                    "Cara penggunaan:\n" \
-                    "1. Bergabung dengan channel kami.\n" \
-                    "2. Kirim pesan yang diteruskan dari channel/grup yang ingin Anda ketahui ID-nya."
-    
-    # Pesan force subs
-    FORCE_SUBS_MESSAGE = "Silakan bergabung dengan channel kami untuk menggunakan bot ini: {channel_link}"
-    
-    # ID channel untuk force subs (jika ada)
-    FORCE_SUBS_CHANNEL_ID = '@AlteregoNetwork'  # Ganti dengan ID channel Anda (opsional)
+API_ID = 23795091
+API_HASH = "64a8dadb2a928e4a97e037e834d1f8db"
+BOT_TOKEN = "6196900445:AAFEhGC7_qBF0Q5PDuiuwicf1l79GRCwTlE"
+OWNER_ID = 6109394253
+
+START_MSG = "Halo! Saya adalah bot yang dapat mengambil ID channel/grup dari pesan yang diteruskan."
+FORCE_SUB_MSG = "Silakan bergabung dengan channel kami sebelum menggunakan bot ini."
