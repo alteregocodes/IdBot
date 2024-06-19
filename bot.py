a38eeb551ef909c9a79e74267bdc8a15a35ee142
/AlteregoCodes <alteregocode@kutmut.my.id>
2024-06-20 05:37:28 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,6 +7,7 @@ import requests
 from pyrogram import Client, filters
 from pyrogram.types import Message, InlineKeyboardButton, InlineKeyboardMarkup
 from config import API_ID, API_HASH, BOT_TOKEN, OWNER_IDS, START_MSG, UPDATE_LOG_FILE
+from pyrogram.errors import PeerIdInvalid
 
 app = Client("channel_id_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_TOKEN)
 
@@ -21,6 +22,8 @@ async def start(client, message: Message):
         for owner_id in OWNER_IDS:
             try:
                 await client.send_message(owner_id, f"Bot telah berhasil diperbarui:\n\n{update_log}")
+            except PeerIdInvalid:
+                print(f"Failed to send message to {owner_id}: PeerIdInvalid")
             except Exception as e:
                 print(f"Failed to send message to {owner_id}: {e}")
         # Hapus file log setelah dikirim
@@ -50,12 +53,10 @@ async def get_forwarded_info(client, message: Message):
 @app.on_message(filters.command("update") & filters.user(OWNER_IDS))
 async def update(client, message: Message):
     await message.reply_text("Bot akan memperbarui dan memulai ulang...")
-    # Hentikan bot
-    app.stop()
     # Lakukan git pull dan simpan hasilnya ke file log sementara
     result = subprocess.run(["git", "pull"], capture_output=True, text=True)
     with open(UPDATE_LOG_FILE, "w") as f:
-        f.write(result.stdout)
+        f.write(result.stdout + "\n" + result.stderr)
     # Jalankan ulang bot
     os.execv(sys.executable, ['python'] + sys.argv)
 
