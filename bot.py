1175e1bb02f1e4718b6c45587965978a86b3e171
/AlteregoCodes <alteregocode@kutmut.my.id>
2024-06-19 21:20:12 +0700

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,5 +1,8 @@
 # bot.py
 
+import os
+import sys
+import subprocess
 from pyrogram import Client, filters
 from pyrogram.types import Message
 from config import API_ID, API_HASH, BOT_TOKEN, OWNER_ID, START_MSG
@@ -16,10 +19,21 @@ async def start(client, message: Message):
 async def get_forwarded_info(client, message: Message):
     if message.forward_from_chat:
         chat_id = message.forward_from_chat.id
-        chat_id_formatted = f"<code> -{abs(chat_id)} </code>" if chat_id < 0 else str(chat_id)
+        chat_id_formatted = f"<code>-100{abs(chat_id)}</code>" if chat_id < 0 else str(chat_id)
         await message.reply_text(f"ID Channel/Grup: {chat_id_formatted}")
     else:
         await message.reply_text("Pesan ini tidak berasal dari channel atau grup.")
+
+# Handler untuk update bot
+@app.on_message(filters.command("update") & filters.user(OWNER_ID))
+async def update(client, message: Message):
+    await message.reply_text("Bot akan memperbarui dan memulai ulang...")
+    # Hentikan bot
+    app.stop()
+    # Lakukan git pull
+    subprocess.run(["git", "pull"])
+    # Jalankan ulang bot
+    os.execv(sys.executable, ['python'] + sys.argv)
 
 if __name__ == "__main__":
     app.run()
