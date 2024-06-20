a185ae5a205f076ba8901d2896226d3a46379b39
/AlteregoCodes <alteregocode@kutmut.my.id>
2024-06-21 06:07:23 +0700

    Update __init__.py

--- module/__init__.py
+++ module/__init__.py
@@ -7,6 +7,7 @@ from .welcome import register_handlers as register_welcome_handlers
 from .tts import register_handlers as register_tts_handlers
 from .help import register_handlers as register_help_handlers
 from .song import register_handlers as register_song_handlers
+from .audteks import register_handlers as register_audteks_handlers
 
 def register_all_handlers(app):
     register_carbon_handlers(app)
@@ -16,4 +17,4 @@ def register_all_handlers(app):
     register_tts_handlers(app)
     register_help_handlers(app)
     register_song_handlers(app)
-    
+    register_audteks_handlers(app)
