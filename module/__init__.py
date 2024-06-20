a47f9d413fe0ad5a1439128fab1f9d80544ebc74
/AlteregoCodes <alteregocode@kutmut.my.id>
2024-06-20 16:22:06 +0700

    Update __init__.py

--- module/__init__.py
+++ module/__init__.py
@@ -4,3 +4,9 @@ from .carbon import *
 from .start import *
 from .getid import *
 from .welcome import *
+
+def register_handlers(app):
+    carbon.register_handlers(app)
+    start.register_handlers(app)
+    getid.register_handlers(app)
+    welcome.register_handlers(app)
