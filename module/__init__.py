c12c0feac4982a0734fd478d14830fe2c5b0a99a
/AlteregoCodes <alteregocode@kutmut.my.id>
2024-06-20 19:53:53 +0700

    Update __init__.py

--- module/__init__.py
+++ module/__init__.py
@@ -6,6 +6,7 @@ from .getid import register_handlers as register_getid_handlers
 from .welcome import register_handlers as register_welcome_handlers
 from .tts import register_handlers as register_tts_handlers
 from .help import register_handlers as register_help_handlers
+from .update import update_command  # Import update_command from update.py
 
 def register_all_handlers(app):
     register_carbon_handlers(app)
@@ -14,3 +15,6 @@ def register_all_handlers(app):
     register_welcome_handlers(app)
     register_tts_handlers(app)
     register_help_handlers(app)
+
+    # Register the update command
+    app.add_handler(update_command)
