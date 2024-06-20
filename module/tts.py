1695bc0b9621341bfabc5b27f7d51281a0c2a98a
/AlteregoCodes <alteregocode@kutmut.my.id>
2024-06-20 08:52:17 +0700

    Update tts.py

--- module/tts.py
+++ module/tts.py
@@ -1,3 +1,5 @@
+# module/tts.py
+
 from gtts import gTTS
 from io import BytesIO
 import os
@@ -18,14 +20,20 @@ LANG_CODES = {
 
 DEFAULT_LANG = "id"  # Bahasa Inggris sebagai default
 
+# Dictionary untuk menyimpan preferensi bahasa pengguna
+USER_LANG_PREFS = {}
+
 def text_to_speech(text, language):
     tts = gTTS(text, lang=language)
     output_file = "output.ogg"
     tts.save(output_file)
     return output_file
 
-def get_lang_code(language):
-    return LANG_CODES.get(language, DEFAULT_LANG)
+def get_lang_code(user_id):
+    return USER_LANG_PREFS.get(user_id, DEFAULT_LANG)
+
+def set_lang_preference(user_id, language_code):
+    USER_LANG_PREFS[user_id] = language_code
 
 def remove_output_file(file_path):
     if os.path.exists(file_path):
