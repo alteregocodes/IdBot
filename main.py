93f552117327379942d3aaa81091b14e192f2ea5
/AlteregoCodes <alteregocode@kutmut.my.id>
2024-06-20 09:33:32 +0700

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ app = Client("channel_id_bot", api_id=API_ID, api_hash=API_HASH, bot_token=BOT_T
 
 # Impor semua handler dari pluggins/handler.py
 from pluggins.handler import *
+from module import *
 
 # Menampilkan pesan saat bot dijalankan
 print("Bot telah dijalankan, apabila butuh bantuan chat @SayaKyu")
