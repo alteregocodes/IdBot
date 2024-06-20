d9899007e20cd2b8908d146f75092b1b54cb8f6f
/AlteregoCodes <alteregocode@kutmut.my.id>
2024-06-20 16:27:44 +0700

    Update __init__.py

--- module/__init__.py
+++ module/__init__.py
@@ -1,12 +1,14 @@
 # module/__init__.py
 
-from .carbon import register_handlers as carbon_register_handlers
-from .start import register_handlers as start_register_handlers
-from .getid import register_handlers as getid_register_handlers
-from .welcome import register_handlers as welcome_register_handlers
+from .carbon import *
+from .start import *
+from .getid import *
+from .welcome import *
+from .tts import *
 
 def register_all_handlers(app):
     carbon_register_handlers(app)
     start_register_handlers(app)
     getid_register_handlers(app)
     welcome_register_handlers(app)
+    tts_register_handlers(app)
