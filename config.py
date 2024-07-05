5da30490340efb576f4929de21dce48d060fbb2a
/AlteregoCodes <alteregocode@kutmut.my.id>
2024-07-05 07:58:23 +0700

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ API_HASH = "64a8dadb2a928e4a97e037e834d1f8db"
 BOT_TOKEN = "7283082118:AAEmmoOO3UhxIEXgoVGJV2wiiujuOygFz0k"
 
 OWNER_IDS = [7494727691]  # Hanya satu owner ID
-MONGO_URI = ""
+MONGO_URI = "mongodb+srv://reacode:<password>@reamusic.x7hyx2h.mongodb.net/?retryWrites=true&w=majority&appName=reamusic"
 
 START_MSG = "Halo! Saya adalah bot yang dapat mengambil ID channel/grup dari pesan yang diteruskan."
 UPDATE_LOG_FILE = "update_log.txt"  # Nama file log pembaruan sementara
