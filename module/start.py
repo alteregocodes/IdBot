bc26d43981d2cbae0fc26ae5568d5e4a7be49a79
/AlteregoCodes <alteregocode@kutmut.my.id>
2024-07-07 20:35:28 +0700

    Update start.py

--- module/start.py
+++ module/start.py
@@ -12,7 +12,7 @@ def register_handlers(app):
                 InlineKeyboardButton("Support Channel", url="https://t.me/Alteregonetwork"),
                 InlineKeyboardButton("Support Grup", url="https://t.me/Alterego_ID")
             ],
-            [InlineKeyboardButton("Ambil String", callback_data="ambil_string")]
+            [InlineKeyboardButton("Ambil String", callback_data="get_string")]
         ]
         reply_markup = InlineKeyboardMarkup(buttons)
         await message.reply_text(START_MSG, reply_markup=reply_markup)
@@ -20,22 +20,3 @@ def register_handlers(app):
     @app.on_message(filters.command("start") & filters.group)
     async def start_group(client, message: Message):
         await message.reply_text("Halo! Saya adalah bot yang dapat mengambil ID channel/grup dari pesan yang diteruskan.")
-    
-    @app.on_callback_query(filters.regex("ambil_string"))
-    async def string_callback(client, callback_query):
-        ask_ques = "**☞︎︎︎ ᴄʜᴏᴏsᴇ ᴏɴᴇ ᴛʜᴀᴛ ʏᴏᴜ ᴡᴀɴᴛ ᴛᴏ ɢᴇɴᴇʀᴀᴛᴇ sᴇssɪᴏɴ 𖤍 ✔️ **"
-        buttons_ques = [
-            [
-                InlineKeyboardButton("𝙿𝚈𝚁𝙾𝙶𝚁𝙰𝙼 💗", callback_data="pyrogram"),
-                InlineKeyboardButton("𝙿𝚈𝚁𝙾𝙶𝚁𝙰𝙼 𝚅2 💗", callback_data="pyrogram_v2"),
-            ],
-            [
-                InlineKeyboardButton("𝚃𝙴𝙻𝙴𝚃𝙷𝙾𝙽 💻", callback_data="telethon"),
-            ],
-            [
-                InlineKeyboardButton("𝙿𝚈𝚁𝙾𝙶𝚁𝙰𝙼 𝙱𝙾𝚃 🤖", callback_data="pyrogram_bot"),
-                InlineKeyboardButton("𝚃𝙴𝙻𝙴𝚃𝙷𝙾𝙽 𝙱𝙾𝚃 🤖", callback_data="telethon_bot"),
-            ],
-        ]
-        reply_markup = InlineKeyboardMarkup(buttons_ques)
-        await callback_query.message.reply(ask_ques, reply_markup=reply_markup)
