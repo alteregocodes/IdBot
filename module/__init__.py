7b5b8ce1aad1d84ebcec07ee83e45f835c153b22
/AlteregoCodes <alteregocode@kutmut.my.id>
2024-07-29 09:34:17 +0700

    Update __init__.py

--- module/__init__.py
+++ module/__init__.py
@@ -7,7 +7,7 @@ from .help import register_handlers as register_help_handlers
 from .song import register_handlers as register_song_handlers
 from .audteks import register_handlers as register_audteks_handlers
 from .namebl import register_handlers as register_namebl_handlers
-from .string import register_handlers as register_string_handlers
+from .string import register_handlers
 from .update import register_update_handler  # Mengimpor register_update_handler dari module/update.py
 from .dev import register_handlers as register_dev_handlers  # Mengimpor register_handlers dari module/dev.py
 
